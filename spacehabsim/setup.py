58baa7f342a0658b0ac2d4b5d3dd6a1e2770c68e
tammerb <tammer.barkouki@gmail.com>
2024-05-09 15:15:02 -0700

    add spacehab file to define hab world

--- spacehabsim/setup.py
+++ spacehabsim/setup.py
@@ -2,6 +2,10 @@ from setuptools import find_packages, setup
 
 package_name = 'spacehabsim'
 
+install_requires = [
+    "pyrobosim",
+]
+
 setup(
     name=package_name,
     version='0.0.0',
@@ -11,15 +15,16 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=install_requires,
     zip_safe=True,
     maintainer='tammer',
     maintainer_email='tammer.barkouki@gmail.com',
-    description='TODO: Package description',
+    description='Space Habitat world specification for pyrobosim',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'spacedemo = spacehabsim.spacehab:main',
         ],
     },
 )
