7a5eeb29f87f746fb5019f531228a21e27b3df4c
tammerb <tammer.barkouki@gmail.com>
2024-05-14 15:25:21 -0700

    create local data dir for locations and objects

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'spacehabsim'
 
@@ -14,6 +16,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share/', package_name, 'data'), glob('data/*data.yaml')),
     ],
     install_requires=install_requires,
     zip_safe=True,
--- spacehabsim/spacehab.py
+++ spacehabsim/spacehab.py
@@ -12,22 +12,47 @@ import numpy as np
 from pyrobosim.core import Robot, World, WorldYamlLoader
 from pyrobosim.gui import start_gui
 from pyrobosim.navigation import ConstantVelocityExecutor, PathPlanner
-from pyrobosim.utils.general import get_data_folder
+# from pyrobosim.utils.general import get_data_folder
 from pyrobosim.utils.pose import Pose
 from pyrobosim_ros.ros_interface import WorldROSWrapper
+
+def get_data_folder(): # TODO: Move this into a utils folder
+    """
+    Get a path to the folder containing data.
+
+    If running using ``ros2 run``, this looks at the installed data in the
+    ``pyrobosim`` colcon package's share directory.
+
+    If running standalone, this looks at the data folder in the actual source.
+
+    :return: Path to data folder.
+    :rtype: str
+    """
+    try:
+        # If running as a ROS 2 node, get the data folder from the package share directory.
+        from ament_index_python.packages import get_package_share_directory
+
+        data_folder = os.path.join(get_package_share_directory("spacehabsim"), "data")
+    except:
+        # Else, assume it's relative to the file's current directory.
+        data_folder = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "..", "data"
+        )
+
+    return data_folder
 
 
 data_folder = get_data_folder()
 
 
 def create_world():
-    """Create a test world"""
+    """Create a space habitat world"""
     world = World()
 
     # Set the location and object metadata (TODO: Make new folders and files)
     world.set_metadata(
-        locations=os.path.join(data_folder, "example_location_data.yaml"),
-        objects=os.path.join(data_folder, "example_object_data.yaml"),
+        locations=os.path.join(data_folder, "location_data.yaml"),
+        objects=os.path.join(data_folder, "object_data.yaml"),
     )
 
     # Add rooms
